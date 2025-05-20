eeaee50b9e0a454baf1bfe108675c006cf9ea52f
Jirka Neumann <jirka.neumann@gmx.de>
2025-05-21 01:16:00 +0200

    template

---
+++ Py-SQL-check2insert.py
@@ -0,0 +1,27 @@
+from jne import prinje
+
+try:
+  with open("jne.txt", "r", encoding="utf-8") as notes:
+    getnotes = []
+    for lines in notes:
+      getnotes.append(lines.strip("\r\n"))
+    notes.close()
+    path = getnotes[0]
+    path = '/'.join(path.split('\\'))
+except:
+  path = ""
+  try: import ctypes; ctypes.windll.user32.MessageBoxW(0, "check path...", "Python", 1)
+  except: print("check path...")
+
+
+#--- read sql
+
+#--- read csv
+
+#--- check with chromadb
+
+#--- update sql
+
+#--- update csv
+
+
