bb15b74142267823fb19a443ae7e3ff69ced4179
Jirka Neumann <jirka.neumann@gmx.de>
2026-04-18 22:35:45 +0200

    update

--- Py-SQL-check2insert.py
+++ Py-SQL-check2insert.py
@@ -40,8 +40,7 @@ conn = psycopg2.connect(database=dbpass[0],
 
 cur = conn.cursor()
 
-#cur.execute('SELECT * FROM bands')
-cur.execute('SELECT name FROM bands')
+cur.execute('SELECT name FROM customers')
 sql_data = cur.fetchall()
 #conn.commit()
 cur.close()
@@ -213,8 +212,8 @@ cur = conn.cursor()
 for i in range(len(ready4db)):
     val1 = ready4db[i]
     val2 = "Python"
-    cur.execute('INSERT into bands (name, genre) VALUES (%s, %s)', (val1, val2))
-#cur.executemany('INSERT into bands (name) VALUES (%s)', ready4db)
+    cur.execute('INSERT into customers (name, genre) VALUES (%s, %s)', (val1, val2))
+#cur.executemany('INSERT into customers (name) VALUES (%s)', ready4db)
 conn.commit()
 cur.close()
 conn.close()
